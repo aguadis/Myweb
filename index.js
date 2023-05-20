124298aa7713b328776853a7714ebde24349eba3
aguadis <guadi1404@gmail.com>
2023-05-20 13:58:33 -0300

    agrego carousel

--- index.js
+++ index.js
@@ -24,4 +24,49 @@ function validateForm() {
     } else {
         alert('Correo electrónico enviado correctamente.');
     }
-}       
+}  
+/*
+const slider = document.querySelector('#slider');
+let sliderSection = document.querySelectorAll('.slider__section');
+let sliderSectionLast = sliderSection[sliderSection.length - 1];
+
+const letf = document.querySelector('#letf');
+const rigth = document.querySelector('#rigth');
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function next() {
+    let sliderSectionFirst = document.querySelectorAll('.slider__section')[0];
+    slider.style.marginLeft = '-200%';
+    slider.style.transition = 'all 0.5s ease';
+    setTimeout(function(){
+        slider.style.transition = 'none';
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = '-100%';
+    }, 500);
+}
+
+function prev() {
+    let sliderSection = document.querySelectorAll('.slider__section');
+    let sliderSectionLast = sliderSection[sliderSection.length - 1];
+    slider.style.marginLeft = '0%';
+    slider.style.transition = 'all 0.5s ease';
+    setTimeout(function(){
+        slider.style.transition = 'none';
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.marginLeft = '-100%';
+    }, 500);
+}
+
+rigth.addEventListener('click', function(){
+    next();
+});
+
+left.addEventListener('click', function(){
+    prev();
+});
+
+setInterval(function(){
+    next();
+}, 5000);
+*/      
